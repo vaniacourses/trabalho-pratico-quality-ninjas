20f299b8e42a5f331a28c96350e172d3c6bc2c86
katematheus <kmatheus@id.uff.br>
2023-06-21 23:40:36 -0300

    teste da classe singularIndex incompleto

---
+++ BracketPair/__tests__/singularIndex.test.ts
@@ -0,0 +1,20 @@
+import SingularIndex from "../src/singularIndex";
+import BracketPair from "./bracketPair";
+import ColorIndexes from "./IColorIndexes";
+import expect for expect;
+
+describe("singularIndex", () => {
+    it("deve capturar o index da próxima posição", () => {
+      const singularIndex= new SingularIndex;
+      const bracketPair = new BracketPair();
+       
+    // Abre colchetes
+        singularIndex.getOpenBrackets();
+
+    // Captura index da próxima posição
+       singularIndex.getPreviousIndex[bracketPair].push(()=> {
+       });
+
+    //Configura a cor do colchete
+       singularIndex.setCurrent()
+}}
